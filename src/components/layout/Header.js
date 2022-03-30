924dd7bbcfa8ecc2cb5ff6a420899fc2dc076975
Florian <hi@florianpanchout.com>
2022-03-30 12:06:24 +0200

    Remove Marianne

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,8 +13,6 @@ export default function Header() {
   return (
     <Wrapper>
       <HeaderBody>
-        <Logo splitCharacter={10}>République Française</Logo>
-
         <Service
           title='Verdir ma flotte'
           description={`L’outil d'aide à la décision pour l'achat de flottes de véhicules de transport de marchandises.`}
