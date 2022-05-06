4d7cd74ab0cf813f74568985c4163b9f5ff5cfe2
Florian <hi@florianpanchout.com>
2022-05-06 10:11:20 +0200

    Add options case

--- netlify/functions/getTruckComparison.js
+++ netlify/functions/getTruckComparison.js
@@ -2,6 +2,16 @@ const axios = require('axios')
 
 exports.handler = function (event) {
   console.log(event.body)
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: '204',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+      },
+    }
+  }
   return axios
     .post(
       `https://mobicloud.ifpen.com/tco2/service/v1/truckComparison`,
