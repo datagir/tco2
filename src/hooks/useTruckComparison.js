7f89beec2ec68971d2144c19a8eb8350cf512495
Florian <hi@florianpanchout.com>
2022-05-06 10:26:09 +0200

    add stringify (again)

--- src/hooks/useTruckComparison.js
+++ src/hooks/useTruckComparison.js
@@ -6,26 +6,28 @@ export default function useTruckComparison(search) {
   return useQuery(
     ['truckComparison', token],
     () =>
-      axios
-        .post(
-          `https://staging--tco2.netlify.app/.netlify/functions/getTruckComparison?token=${token}`,
-          {
-            vehicle: { vehicleCategory: 'RIGIDTRUCK-12T' },
-            use: {
-              operatingRange: 'URBAN',
-              usesRepartition: [20, 30, 50],
-              OriginDestination: {
-                origin: { latitude: null, longitude: null },
-                destination: { latitude: null, longitude: null },
-              },
-              totalAnnualDistance: 100000,
-              payload: 60,
-            },
-            tcoParameters: { possessionDuration: 10, fuelConsumption: 0 },
-            echartsConfiguration: true,
-          }
-        )
-        .then((res) => res.data),
+      token
+        ? axios
+            .post(
+              `https://staging--tco2.netlify.app/.netlify/functions/getTruckComparison?token=${token}`,
+              JSON.stringify({
+                vehicle: { vehicleCategory: 'RIGIDTRUCK-12T' },
+                use: {
+                  operatingRange: 'URBAN',
+                  usesRepartition: [20, 30, 50],
+                  OriginDestination: {
+                    origin: { latitude: null, longitude: null },
+                    destination: { latitude: null, longitude: null },
+                  },
+                  totalAnnualDistance: 100000,
+                  payload: 60,
+                },
+                tcoParameters: { possessionDuration: 10, fuelConsumption: 0 },
+                echartsConfiguration: true,
+              })
+            )
+            .then((res) => res.data)
+        : Promise.resolve([]),
     {
       keepPreviousData: true,
       refetchOnWindowFocus: false,
