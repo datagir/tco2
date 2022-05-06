fdfee2134a3b40b48bc32cd198fc0a78f3e71049
Florian <hi@florianpanchout.com>
2022-05-06 10:00:52 +0200

    Dynamic body

--- netlify/functions/getTruckComparison.js
+++ netlify/functions/getTruckComparison.js
@@ -1,11 +1,10 @@
 const axios = require('axios')
 
 exports.handler = function (event) {
-  console.log(event)
   return axios
     .post(
       `https://mobicloud.ifpen.com/tco2/service/v1/truckComparison`,
-      '{"vehicle":{"vehicleCategory":"RIGIDTRUCK-12T"},"use":{"operatingRange":"URBAN","usesRepartition":[20,30,50],"OriginDestination":{"origin":{"latitude":null,"longitude":null},"destination":{"latitude":null,"longitude":null}},"totalAnnualDistance":100000,"payload":60},"tcoParameters":{"possessionDuration":10,"fuelConsumption":0},"echartsConfiguration":true}',
+      event.body,
       {
         headers: {
           Authorization: 'Bearer ' + event.queryStringParameters.token,
