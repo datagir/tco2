56b6a5a85ef9edcadb0874e96c67155fa455446a
Florian <hi@florianpanchout.com>
2022-05-06 10:03:45 +0200

    Add stringify

--- netlify/functions/getTruckComparison.js
+++ netlify/functions/getTruckComparison.js
@@ -4,7 +4,7 @@ exports.handler = function (event) {
   return axios
     .post(
       `https://mobicloud.ifpen.com/tco2/service/v1/truckComparison`,
-      event.body,
+      JSON.stringify(event.body),
       {
         headers: {
           Authorization: 'Bearer ' + event.queryStringParameters.token,
