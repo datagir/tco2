1b87d907c6abf728d90aaa60bfbf0a912d50dae5
Florian <hi@florianpanchout.com>
2022-05-10 11:30:14 +0200

    Arrow

--- src/views/home/search/Vehicle.js
+++ src/views/home/search/Vehicle.js
@@ -32,6 +32,17 @@ const ToggleButton = styled.button`
   &:hover {
     text-decoration: underline;
   }
+
+  svg {
+    margin-bottom: 0.125rem;
+    margin-left: 0.25rem;
+    transform: rotate(${(props) => (props.open ? 180 : 0)}deg);
+    transition: transform 200ms ease-out;
+
+    path {
+      fill: ${(props) => props.theme.colors.main};
+    }
+  }
 `
 const StyledSelect = styled(Select)`
   max-width: 14rem;
@@ -54,8 +65,14 @@ export default function Vehicle() {
     <Wrapper>
       <Header>
         <Title for='vehicleCategory'>Catégorie véhicule</Title>
-        <ToggleButton onClick={() => setOpen((prevOpen) => !prevOpen)}>
+        <ToggleButton
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+          open={open}
+        >
           Voir {open ? 'moins' : 'plus'} d'option véhicule
+          <svg width='10' height='6' viewBox='0 0 10 6' fill='none'>
+            <path d='M4.99997 5.85012C4.82075 5.85012 4.64155 5.78169 4.50491 5.64512L0.205141 1.3453C-0.0683804 1.07178 -0.0683804 0.628311 0.205141 0.3549C0.478552 0.0814886 0.921932 0.0814886 1.19548 0.3549L4.99997 4.15961L8.80449 0.355032C9.07801 0.0816214 9.52134 0.0816214 9.79473 0.355032C10.0684 0.628443 10.0684 1.07191 9.79473 1.34543L5.49503 5.64525C5.35832 5.78184 5.17912 5.85012 4.99997 5.85012Z' />
+          </svg>
         </ToggleButton>
       </Header>
       <StyledSelect
