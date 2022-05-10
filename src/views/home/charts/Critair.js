5e422f772a2ed3d1d8177a022793debdb187f1ae
Florian <hi@florianpanchout.com>
2022-05-10 15:08:20 +0200

    Issue 4

--- src/views/home/charts/Critair.js
+++ src/views/home/charts/Critair.js
@@ -7,7 +7,7 @@ import List from './critair/List'
 const Wrapper = styled.div`
   position: relative;
   margin-bottom: 0.5rem;
-  padding: 1.5rem 2rem;
+  padding: 1.5rem;
   background-color: ${(props) => props.theme.colors.secondLight};
   border-radius: 1rem;
 
