2f7b0205fa65fad1b6bfdf992ede149b837f1f99
ifpen-chassea <43041313+ifpen-chassea@users.noreply.github.com>
2022-10-06 07:23:17 +0200

    Test

--- src/hooks/useTruckComparison.js
+++ src/hooks/useTruckComparison.js
@@ -65,7 +65,7 @@ export default function useTruckComparison() {
                     resaleCost: costs[vehicleTechnology].resaleCost,
                   })),
                 },
-                chartsConfiguration: false,
+                echartsConfiguration: false,
               }
             )
             .then((res) => res.data)
