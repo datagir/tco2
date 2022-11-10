2a7b73affdd52edf157e331b41e7651b1a6d78e5
Daniel Mayer <daniel.mayer@tech-advantage.com>
2022-11-10 09:50:19 +0100

    Ajout d'un bouton d'affichage pour la section consommation

--- src/views/home/charts/usage-visualization/FuelConsumption.js
+++ src/views/home/charts/usage-visualization/FuelConsumption.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TextInput from '../../../../components/base/TextInput';
 import SearchContext from '../../../../utils/SearchContext';
+import ToggleButton from '../../../../components/base/ToggleButton';
 
 const UsageDescription = styled.div`
   margin: 2rem 0 0 1.5rem;
+  button {
+    float: right;
+    padding: 1rem;
+  }
 `
 const MainTextInput = styled(TextInput)`
   max-width: 8rem;
@@ -17,6 +22,7 @@ const Result = styled.span`
   background-color: ${(props) => props.theme.colors.footerLight};
   border-radius: .5rem;
   padding: 0.5rem;
+  margin-right: .25rem;
   line-height: 2em;
 `
 
@@ -24,18 +30,27 @@ export function FuelConsumption(props) {
   const { fuelConsumption, setFuelConsumption } = useContext(SearchContext)
   const { meanFuelConsumption } = props
 
+  const [open, setOpen] = useState(false)
+
   return (
     <UsageDescription>
-      <strong>La consommation de référence calculée pour ce véhicule est de <Result>{meanFuelConsumption || 0}l/100km</Result></strong>
+      <strong>La consommation de référence calculée pour ce véhicule est
+        de <Result>{ meanFuelConsumption || 0 }</Result>l/100km.</strong>
+      <ToggleButton
+        onToggle={ setOpen }
+        open={ open }
+        title={ 'Je connais ma consommation' }/>
       <br/><br/>
-      <p>Si vous connaissez votre consommation de référence d’un véhicule roulant au diesel B7 vous pouvez la modifier
-        pour ajuster les consommations des autres énergies.</p>
-      <MainTextInput
-        name='fuelConsumption'
-        unit={ 'l/100km' }
-        value={ fuelConsumption || 0 }
-        onChange={({ value }) => setFuelConsumption(value)}
-      />
+      { open && (<div>
+        <p>Si vous connaissez votre consommation de référence d’un véhicule roulant au diesel B7 vous pouvez la modifier
+          pour ajuster les consommations des autres énergies.</p>
+        <MainTextInput
+          name='fuelConsumption'
+          unit={ 'l/100km' }
+          value={ fuelConsumption || 0 }
+          onChange={ ({ value }) => setFuelConsumption(value) }
+        />
+      </div>) }
     </UsageDescription>
   )
 }
--- src/views/home/search/Vehicle.js
+++ src/views/home/search/Vehicle.js
@@ -7,6 +7,7 @@ import Select from 'components/base/Select'
 import Costs from './vehicle/Costs'
 import { useHistory } from 'react-router-dom';
 import StyleContext from '../../../utils/StyleContext';
+import ToggleButton from '../../../components/base/ToggleButton';
 
 const Wrapper = styled.div`
   position: relative;
@@ -30,32 +31,6 @@ const Flex = styled.div`
 const Title = styled.label`
   display: block;
   margin-bottom: 1.125rem;
-`
-const ToggleButton = styled.button`
-  font-size: 0.875rem;
-  color: ${(props) => props.theme.colors.main};
-  background: none;
-  border: none;
-  cursor: pointer;
-
-  &:hover {
-    text-decoration: underline;
-  }
-
-  svg {
-    margin-bottom: 0.125rem;
-    margin-left: 0.25rem;
-    transform: rotate(${(props) => (props.open ? 180 : 0)}deg);
-    transition: transform 200ms ease-out;
-
-    path {
-      fill: ${(props) => props.theme.colors.main};
-    }
-  }
-
-  ${(props) => props.theme.mq.small} {
-    display: none;
-  }
 `
 const StyledSelect = styled(Select)`
   min-width: 18rem;
@@ -88,14 +63,10 @@ export default function Vehicle() {
           ))}
         </StyledSelect>
         <ToggleButton
-          onClick={() => setOpen((prevOpen) => !prevOpen)}
+          onToggle={setOpen}
           open={open}
-        >
-          Je renseigne mes coûts véhicule
-          <svg width='10' height='6' viewBox='0 0 10 6' fill='none'>
-            <path d='M4.99997 5.85012C4.82075 5.85012 4.64155 5.78169 4.50491 5.64512L0.205141 1.3453C-0.0683804 1.07178 -0.0683804 0.628311 0.205141 0.3549C0.478552 0.0814886 0.921932 0.0814886 1.19548 0.3549L4.99997 4.15961L8.80449 0.355032C9.07801 0.0816214 9.52134 0.0816214 9.79473 0.355032C10.0684 0.628443 10.0684 1.07191 9.79473 1.34543L5.49503 5.64525C5.35832 5.78184 5.17912 5.85012 4.99997 5.85012Z' />
-          </svg>
-        </ToggleButton>
+          title={'Je renseigne mes coûts véhicule'}
+        />
       </Flex>
       <Costs open={open} />
     </Wrapper>
