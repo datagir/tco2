c127351563e80f203f7890ff60589059ad3c1694
Daniel Mayer <daniel.mayer@tech-advantage.com>
2022-11-18 09:49:51 +0100

    - Vehicle comparison web service body request: Parses string null values

--- src/hooks/useTruckComparison.js
+++ src/hooks/useTruckComparison.js
@@ -6,6 +6,7 @@ import SearchContext from 'utils/SearchContext'
 import useDebounce from './useDebounce';
 import { isEmpty, isNil } from '../utils/globalUtils';
 import useTruckDefaultSettings from './useTruckDefaultSettings';
+import { parseString } from '../utils/numberUtils';
 
 const areLocationsReady = (start, end) => {
   const locations = [start?.longitude, start?.latitude, end?.longitude, end?.latitude];
@@ -72,20 +73,20 @@ export default function useTruckComparison() {
                       longitude: end?.longitude || null,
                     },
                   },
-                  totalAnnualDistance: debouncedTotalAnnualDistance,
-                  payload: debouncedPayload,
+                  totalAnnualDistance: parseString(debouncedTotalAnnualDistance),
+                  payload: parseString(debouncedPayload),
                 },
                 tcoParameters: {
-                  possessionDuration: debouncedPossessionDuration,
-                  fuelConsumption: debouncedFuelConsumption,
+                  possessionDuration: parseString(debouncedPossessionDuration),
+                  fuelConsumption: parseString(debouncedFuelConsumption),
                   costs: Object.keys(debouncedCosts).map((vehicleTechnology) => ({
                     vehicleTechnology,
-                    purchaseCost: debouncedCosts[vehicleTechnology].purchaseCost,
-                    purchaseGrant: debouncedCosts[vehicleTechnology].purchaseGrant,
-                    maintenanceCost: debouncedCosts[vehicleTechnology].maintenanceCost,
-                    insuranceCost: debouncedCosts[vehicleTechnology].insuranceCost,
-                    resaleCost: debouncedCosts[vehicleTechnology].resaleCost,
-                    energyCost: debouncedCosts[vehicleTechnology].energyCost,
+                    purchaseCost: parseString(debouncedCosts[vehicleTechnology].purchaseCost),
+                    purchaseGrant: parseString(debouncedCosts[vehicleTechnology].purchaseGrant),
+                    maintenanceCost: parseString(debouncedCosts[vehicleTechnology].maintenanceCost),
+                    insuranceCost: parseString(debouncedCosts[vehicleTechnology].insuranceCost),
+                    resaleCost: parseString(debouncedCosts[vehicleTechnology].resaleCost),
+                    energyCost: parseString(debouncedCosts[vehicleTechnology].energyCost),
                   })),
                 },
                 chartsConfiguration: false,
